eaa448be0c4b2531934b2c6497c506387633ffbc
Saker Hossain <70207555+Saker-Hossain@users.noreply.github.com>
2023-12-04 17:15:49 +0600

    Admin controller folder with category , order, product, subcategory controller with function

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function Index(){
+        return view('admin.allcategory');
+    }
+
+    public function AddCategory(){
+        return view('admin.addcategory');
+    }
 }
--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -8,4 +8,11 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     //
+    public function Index(){
+        return view('admin.allproducts');
+    }
+
+    public function AddProduct(){
+        return view('admin.addproduct');
+    }
 }
