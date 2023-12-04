354f5207ea0397f76f59bf4a1b33e5839ad016b0
Saker Hossain <70207555+Saker-Hossain@users.noreply.github.com>
2023-12-04 16:54:28 +0600

    Admin controller folder with category , order, product, subcategory controller with routes.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SubCategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -31,9 +25,30 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function(){
+
     Route::controller(DashboardController::class)->group(function(){
         Route::get('/admin/dashboard', 'Index');
     });
+
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/admin/all-category', 'Index')->name('allcategory');
+        Route::get('/admin/add-category', 'AddCategory')->name('addcategory');
+    });
+
+    Route::controller(SubCategoryController::class)->group(function(){
+        Route::get('/admin/all-subcategory', 'Index')->name('allsubcategory');
+        Route::get('/admin/add-subcategory', 'AddSubCategory')->name('addsubcategory');
+    });
+
+    Route::controller(ProductController::class)->group(function(){
+        Route::get('/admin/all-product', 'Index')->name('allproduct');
+        Route::get('/admin/add-product', 'AddProduct')->name('addproduct');
+    });
+
+    Route::controller(OrderController::class)->group(function(){
+        Route::get('/admin/pending-order', 'Index')->name('pendingorder');
+    });
+
 });
 
 
