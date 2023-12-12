867aa192d8ee912e201fdf2c9080f66ef69fc8b7
Saker Hossain <70207555+Saker-Hossain@users.noreply.github.com>
2023-12-12 16:07:09 +0600

    All product page setup with table

--- resources/views/admin/allproducts.blade.php
+++ resources/views/admin/allproducts.blade.php
@@ -3,5 +3,41 @@
 All Product
 @endsection
 @section('content')
-hellow from all product
+<div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Pages/</span> All Product</h4>
+    <!-- Bootstrap Table with Header - Light -->
+    <div class="card">
+        <h5 class="card-header">Available All Product Information</h5>
+        <div class="table-responsive text-nowrap">
+            <table class="table">
+                <thead class="table-light">
+                    <tr>
+                        <th>Id</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Cost</th>
+                        <th>Qty</th>
+                        <th>Image</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody class="table-border-bottom-0">
+                    <tr>
+                        <td>1</td>
+                        <td>fan</td>
+                        <td>20</td>
+                        <td>100</td>
+                        <td>14</td>
+                        <td></td>
+                        <td>
+                            <a href="" class="btn btn-primary">Edit</a>
+                            <a href="" class="btn btn-warning">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
+
