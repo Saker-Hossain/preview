cf8ddc6a1dbc62bba11dd61490e1593aebff09c0
Saker Hossain <70207555+Saker-Hossain@users.noreply.github.com>
2023-12-16 15:39:37 +0600

    Fetching Category and Sub Category For Add Product Page

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Subcategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,6 +15,9 @@ class ProductController extends Controller
     }
 
     public function AddProduct(){
-        return view('admin.addproduct');
+
+        $categories = Category::latest()->get();
+        $subcategories = Subcategory::latest()->get();
+        return view('admin.addproduct', compact('categories', 'subcategories'));
     }
 }
