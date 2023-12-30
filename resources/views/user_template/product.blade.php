a185d38e5940c9c76ebb7d2b92f8ec0ae2811f77
Saker Hossain <70207555+Saker-Hossain@users.noreply.github.com>
2023-12-30 12:48:18 +0600

    Creating Single Product Display Page Along With Related Products

--- resources/views/user_template/product.blade.php
+++ resources/views/user_template/product.blade.php
@@ -1,4 +1,101 @@
 @extends('user_template.layouts.template')
 @section('main-content')
-<h2>product page</h2>
+<section class="single_product_details_area d-flex align-items-center">
+
+    <!-- Single Product Thumb -->
+    <div class="single_product_thumb clearfix">
+        <div class="product_thumbnail_slides owl-carousel">
+            <img src="img/product-img/product-big-1.jpg" alt="">
+            <img src="img/product-img/product-big-2.jpg" alt="">
+            <img src="img/product-img/product-big-3.jpg" alt="">
+        </div>
+    </div>
+
+    <!-- Single Product Description -->
+    <div class="single_product_desc clearfix">
+        <span>mango</span>
+        <a href="cart.html">
+            <h2>One Shoulder Glitter Midi Dress</h2>
+        </a>
+        <p class="product-price"><span class="old-price">$65.00</span> $49.00</p>
+        <p class="product-desc">Mauris viverra cursus ante laoreet eleifend. Donec vel fringilla ante. Aenean finibus velit id urna vehicula, nec maximus est sollicitudin.</p>
+
+        <!-- Form -->
+        <form class="cart-form clearfix" method="post">
+            <!-- Select Box -->
+            <div class="select-box d-flex mt-50 mb-30">
+                <select name="select" id="productSize" class="mr-5">
+                    <option value="value">Size: XL</option>
+                    <option value="value">Size: X</option>
+                    <option value="value">Size: M</option>
+                    <option value="value">Size: S</option>
+                </select>
+                <select name="select" id="productColor">
+                    <option value="value">Color: Black</option>
+                    <option value="value">Color: White</option>
+                    <option value="value">Color: Red</option>
+                    <option value="value">Color: Purple</option>
+                </select>
+            </div>
+            <!-- Cart & Favourite Box -->
+            <div class="cart-fav-box d-flex align-items-center">
+                <!-- Cart -->
+                <button type="submit" name="addtocart" value="5" class="btn essence-btn">Add to cart</button>
+                <!-- Favourite -->
+                <div class="product-favourite ml-4">
+                    <a href="#" class="favme fa fa-heart"></a>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+<div class="new_arrivals_area section-padding-80 clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-heading text-center">
+                    <h2>Related Products</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="popular-products-slides owl-carousel">
+
+                    <!-- Single Product -->
+                    <div class="single-product-wrapper">
+                        <!-- Product Image -->
+                        <div class="product-img">
+                            <img src="home/img/product-img/product-2.jpg" alt="">
+                            <!-- Hover Thumb -->
+                            {{-- <img class="hover-img" src="{{asset('home/img/product-img/product-2.jpg')}}" alt="">
+                            --}}
+                            <!-- Favourite -->
+                            <div class="product-favourite">
+                                <a href="#" class="favme fa fa-heart"></a>
+                            </div>
+                        </div>
+                        <div class="product-description">
+                            <span>topshop</span>
+                            <a href="single-product-details.html">
+                                <h6>Product Name</h6>
+                            </a>
+                            <p class="product-price">$ 50</p>
+
+                            <!-- Hover Content -->
+                            <div class="hover-content">
+                                <!-- Add to Cart -->
+                                <div class="add-to-cart-btn">
+                                    <a href="">See More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
